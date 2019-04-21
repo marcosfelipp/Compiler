df49af95faea3bf2bf01cc23006b0c35edbc1f0b
Bruno <brunocesar.oc@hotmail.com>
2019-04-21 00:53:00 -0300

    - carregando dicionario e tabela de transições somente uma vez ao chamar a página - dicionario mais compacto - estado de erro será undefined -  estado de transições de identificador

--- src/js/tabelaSimbolos.js
+++ src/js/tabelaSimbolos.js
@@ -14,95 +14,79 @@ function readFile(that) {
     }
 }
 
+var alphabet = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ").split("");
+var numbers = ("0123456789")
+var dic = {}
+
+for (i in alphabet) {
+    dic[alphabet[i]] = 0
+}
+
+for (i in numbers) {
+    dic[numbers[i]] = 1
+}
+
+dic['.'] = 2
+dic['^'] = 3
+dic['+'] = 4
+dic['-'] = 4
+dic['\"'] = 5
+dic['_'] = 6
+
+var tabela_de_transicao = []
+
+// TODO: Adicionar outros símobolos
+
+//Transições para num
+//tabela_de_transicao[[0, 0]] = -1;
+tabela_de_transicao[[0, 1]] = 1;
+
+//tabela_de_transicao[[1, 0]] = -1;
+tabela_de_transicao[[1, 1]] = 1; //Aceitacao
+tabela_de_transicao[[1, 2]] = 2
+tabela_de_transicao[[1,3]] = 4
+
+//tabela_de_transicao[[2, 0]] = -1
+tabela_de_transicao[[2, 1]] = 3
+
+//tabela_de_transicao[[3, 0]] = -1
+tabela_de_transicao[[3, 1]] = 3 //Aceitacao
+//tabela_de_transicao[[3, 2]] = -1 //Rejeicao
+tabela_de_transicao[[3, 3]] = 4
+
+//tabela_de_transicao[[4, 0]] = -1
+tabela_de_transicao[[4, 1]] = 6
+//tabela_de_transicao[[4, 2]] = -1
+//tabela_de_transicao[[4, 3]] = -1
+tabela_de_transicao[[4, 4]] = 5
+
+//tabela_de_transicao[[5, 0]] = -1
+tabela_de_transicao[[5, 1]] = 6
+
+//tabela_de_transicao[[6, 0]] = -1
+tabela_de_transicao[[6, 1]] = 6 //Aceitacao
+
+//Transições para literal
+tabela_de_transicao[[0, 5]] = 7
+tabela_de_transicao[[7, 5]] = 0 //Aceitacao
+    
+//Transições para identificador
+tabela_de_transicao[[0, 0]] = 8 //Aceitacao
+tabela_de_transicao[[8, 0]] = 8 //Aceitacao
+tabela_de_transicao[[8, 1]] = 8 //Aceitacao
+tabela_de_transicao[[8, 6]] = 8 //Aceitacao
+// Trocar posições com -1 para undefined ou seja nao colocar
+
 function automato(string_lida) {
-    var tabela_de_transicao = []
-
-    // TODO: Adicionar outros símobolos
-    var dic = {
-        "a": 0,
-        "b": 0,
-        "c": 0,
-        "d": 0,
-        "e": 0,
-        "f": 0,
-        "g": 0,
-        "h": 0,
-        "i": 0,
-        "j": 0,
-        "k": 0,
-        "l": 0,
-        "m": 0,
-        "n": 0,
-        "o": 0,
-        "p": 0,
-        "q": 0,
-        "r": 0,
-        "s": 0,
-        "t": 0,
-        "u": 0,
-        "v": 0,
-        "w": 0,
-        "x": 0,
-        "y": 0,
-        "z": 0,
-        "1": 1,
-        "2": 1,
-        "3": 1,
-        "4": 1,
-        "5": 1,
-        "6": 1,
-        "7": 1,
-        "8": 1,
-        "9": 1,
-        "0": 1,
-        ".": 2,
-        "^": 3,
-        "+": 4,
-        "-": 4,
-        "\"": 5
-    }
-
-    // TODO: Adicionar as outras transições:
-
-    //Transições para num
-    tabela_de_transicao[[0, 0]] = -1;
-    tabela_de_transicao[[0, 1]] = 1;
-
-    tabela_de_transicao[[1, 0]] = -1;
-    tabela_de_transicao[[1, 1]] = 1; //Aceitacao
-    tabela_de_transicao[[1, 2]] = 2
-
-    tabela_de_transicao[[2, 0]] = -1
-    tabela_de_transicao[[2, 1]] = 3
-
-    tabela_de_transicao[[3, 0]] = -1
-    tabela_de_transicao[[3, 1]] = 3 //Aceitacao
-    tabela_de_transicao[[3, 2]] = -1 //Rejeicao
-    tabela_de_transicao[[3, 3]] = 4
-
-    tabela_de_transicao[[4, 0]] = -1
-    tabela_de_transicao[[4, 1]] = 6
-    tabela_de_transicao[[4, 2]] = -1
-    tabela_de_transicao[[4, 3]] = -1
-    tabela_de_transicao[[4, 4]] = 5
-
-    tabela_de_transicao[[5, 0]] = -1
-    tabela_de_transicao[[5, 1]] = 6
-
-    tabela_de_transicao[[6, 0]] = -1
-    tabela_de_transicao[[6, 1]] = 6 //Aceitacao
-
-    //Transições para literal
-    tabela_de_transicao[[0, 5]] = 7
-    tabela_de_transicao[[7, 5]] = 0 //Aceitacao
 
     var i = 0
     var state = 0
     var flag = false
 
-    while (string_lida[i] != undefined && state != -1) {
+    while (string_lida[i] != undefined && state != undefined) {
 
-        state = tabela_de_transicao[[state, dic[string_lida[i]]]]
+        state = tabela_de_transicao[[state,dic[string_lida[i]]]]
+        console.log(state,dic[string_lida[i]])
 
         //Tratamento para Literal
         if (state == 7) {
@@ -119,7 +103,6 @@ function automato(string_lida) {
             }
         }
 
-        //console.log(state);
         i += 1;
     }
 
@@ -129,11 +112,14 @@ function automato(string_lida) {
     else if (state == 0 && flag == true) {
         return [string_lida, "literal", "-"]
     }
+    else if (state == 8) {
+        return [string_lida, "id", "-"]
+    }
     else {
         return [string_lida, "ERRO", "-"]
     }
-
 }
+
 
 function inserir_tabela_simbolo(tupla) {
     tabela_de_simbolos[tupla[0]] = [tupla[0], tupla[1], tupla[2]]
@@ -204,3 +190,6 @@ console.log(tabela_de_simbolos)
 // var padrao_AZ = /^[a-zA-Z]*$/
 // var padrao_09 = /^[0-9]*$/
 // console.log(padrao_09.test('1232312746'))
+
+
+
