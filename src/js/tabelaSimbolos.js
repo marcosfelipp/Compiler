967f50befbcf4497217a895643e1fe4e3e2c1bb8
marcosfelipp <mfelipe11@yahoo.com.br>
2019-04-24 18:56:11 -0300

    Correcao de bugs

--- src/js/tabelaSimbolos.js
+++ src/js/tabelaSimbolos.js
@@ -1,122 +1,36 @@
-function readFile(that) {
+function readProg(that) {
     if (that.files && that.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
             var output = e.target.result;
             var output = output.split("\n");
 
-            for (i in output) {
-                inserir_tabela_simbolo([output[i], output[i], "-"])
+            for (linha in output) {
+                // console.log(output[i])
+                addLexemas(output[linha] + '\r', linha)
             }
-
+            //console.log(tabela_de_simbolos)
+            log_simbolos()
         };
         reader.readAsText(that.files[0]);
     }
 }
 
-var alphabet = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ").split("");
-var numbers = ("0123456789")
-var dic = {}
-
-for (i in alphabet) {
-    dic[alphabet[i]] = 0
+function log_simbolos() {
+    texto = ''
+    for (simbolo in tabela_de_simbolos) {
+        texto += "Lexema: " + tabela_de_simbolos[simbolo][0] + '\t' + "token: " + tabela_de_simbolos[simbolo][1] + '\t' + "tipo: " + tabela_de_simbolos[simbolo][2]
+        texto += '\n'
+    }
+    document.getElementById("token").value = texto
 }
 
-for (i in numbers) {
-    dic[numbers[i]] = 1
+function log_erros(linha, coluna){
+    texto = document.getElementById("log_erro").value
+    texto += "ERRO NA LINHA " + linha + ", COLUNA " + coluna + '\n'
+
+    document.getElementById("log_erro").value = texto
 }
-
-dic['.'] = 2
-dic['^'] = 3
-dic['+'] = 4
-dic['-'] = 4
-dic['*'] = 4
-dic['/'] = 4
-dic['\"'] = 5
-dic['_'] = 6
-dic['{'] = 7
-dic['}'] = 8
-dic['\t'] = 9
-dic['\n'] = 10
-dic[' '] = 11
-dic['$'] = 12 // TODO: Fim de arquivo, ver posteriormente
-dic['<'] = 13
-dic['>'] = 14
-dic['='] = 15
-dic['('] = 16
-dic[')'] = 17
-dic[';'] = 18
-
-var tabela_de_transicao = []
-
-// TODO: Adicionar outros símobolos
-
-//Transições para num
-tabela_de_transicao[[0, 1]] = 1;
-
-tabela_de_transicao[[1, 1]] = 1; //Aceitacao
-tabela_de_transicao[[1, 2]] = 2
-tabela_de_transicao[[1, 3]] = 4
-
-tabela_de_transicao[[2, 1]] = 3
-
-tabela_de_transicao[[3, 1]] = 3 //Aceitacao
-tabela_de_transicao[[3, 3]] = 4
-
-tabela_de_transicao[[4, 1]] = 6
-tabela_de_transicao[[4, 4]] = 5
-
-tabela_de_transicao[[5, 1]] = 6
-
-tabela_de_transicao[[6, 1]] = 6 //Aceitacao
-
-//Transições para literal
-tabela_de_transicao[[0, 5]] = 7
-tabela_de_transicao[[7, 5]] = 8 //Aceitacao
-
-//Transições para identificador
-tabela_de_transicao[[0, 0]] = 9 //Aceitacao
-tabela_de_transicao[[9, 0]] = 9 //Aceitacao
-tabela_de_transicao[[9, 1]] = 9 //Aceitacao
-tabela_de_transicao[[9, 6]] = 9 //Aceitacao
-
-//Transições para identificador
-tabela_de_transicao[[0, 7]] = 10
-tabela_de_transicao[[10, 8]] = 11 //Aceitacao
-
-//Transições para Tab, Salto e Espaço, reconhecer, mas ignorar
-tabela_de_transicao[[0, 9]] = 12  //Aceitacao
-tabela_de_transicao[[0, 10]] = 12 //Aceitacao
-tabela_de_transicao[[0, 11]] = 12 //Aceitacao
-
-//Transições EOF
-tabela_de_transicao[[0, 12]] = 13  //Aceitacao
-
-//Transições para Operadores Relacionais < 13, > 14, = 15
-tabela_de_transicao[[0, 13]] = 14  //Aceitação
-tabela_de_transicao[[0, 15]] = 18  //Aceitação
-tabela_de_transicao[[14, 14]] = 15  //Aceitação
-tabela_de_transicao[[0, 14]] = 16  //Aceitação
-tabela_de_transicao[[14, 15]] = 17  //Aceitação
-tabela_de_transicao[[16, 15]] = 17  //Aceitação
-
-//Transições para Atribuições
-tabela_de_transicao[[0, 13]] = 14  //Aceitação
-tabela_de_transicao[[14, 4]] = 19  //Aceitação
-
-//Transições para Operadores Aritméticos (+,-,*,/)
-tabela_de_transicao[[0, 4]] = 20  //Aceitação
-
-//Transições para Abre parenteses
-tabela_de_transicao[[0, 16]] = 21  //Aceitação
-
-//Transições para fecha parenteses
-tabela_de_transicao[[0, 17]] = 22  //Aceitação
-
-//Transições para ponto e vírgula (;)
-tabela_de_transicao[[0, 18]] = 23  //Aceitação
-
-// Trocar posições com -1 para undefined ou seja nao colocar
 
 function automato(string_lida) {
 
@@ -134,7 +48,9 @@ function automato(string_lida) {
                 i++
             }
 
-            state = tabela_de_transicao[[state, dic[string_lida[i - 1]]]]
+            //Se caracter final da string_lida for o abre aspas, então ERRO, caso contrário es
+            i - 1 == 0 ? state = undefined : state = tabela_de_transicao[[state, dic[string_lida[i - 1]]]]
+
             if (state != 8 && state != 11) {
                 state = undefined
             }
@@ -178,78 +94,194 @@ function automato(string_lida) {
     }
     else if (state == 23) {
         return [string_lida, "PT_V", "-"]
-    }
-    else {
+    }else {
         return [string_lida, "ERRO", "-"]
     }
 }
-
-console.log(automato(';'))
 
 function inserir_tabela_simbolo(tupla) {
     tabela_de_simbolos[tupla[0]] = [tupla[0], tupla[1], tupla[2]]
 }
 
-// ALORITMO:
-// Ler simbolo por simbolo
-// Enquanto rejeitar, rejeite
-// Se aceitar, armazene a tupla e continue
-// Se aceitar novamente, armazene a tupla e continue
-// Se rejeitar, adicione na tabela e volte um simbolo e continue
+function addLexemas(strig_lida, linha) {
 
-function addLexemas(line) {
     i = 0
     lex = ''
+    tupla_atual = []
+    while (strig_lida[i] != '\r') {
+        lex += strig_lida[i]
 
-    while (line[i] != '\n') {
-        lex += line[i]
+        // Verifica se está na tabela de simbolos:
+        if (tabela_de_simbolos[lex] != undefined) {
+            //console.log('já está na tabela')
+            if (strig_lida[i+1] == ' '){
+                lex = ''
+                i += 2
+                continue
+            }
+            if(strig_lida[i+1] == '\r'){
+                return
+            }
+            // Porem pode ser um id:
+            retorno = automato(lex + strig_lida[i + 1])
+            //console.log(retorno)
+            if (retorno[1] != 'id') {
+                lex = ''
+                i += 1
+                continue
+            } else {
+                i += 1
+                continue
+            }
 
-        // TODO: Verificar se string já se encontra na tabela de simbolos
+        }
 
         retorno = automato(lex)
-        console.log(retorno)
-
-        if (retorno[1] != "ERRO" && line[i + 1] == " ") {
-            inserir_tabela_simbolo(retorno)
-            lex = ''
-            i += 2
-            continue
-        }
-
-        if (retorno[1] != "ERRO" && line[i + 1] == "\n") {
-            inserir_tabela_simbolo(retorno)
-            return
-        }
 
         if (retorno[1] != "ERRO") {
             tupla_atual = retorno
+            if (strig_lida[i + 1] == " ") {
+                inserir_tabela_simbolo(tupla_atual)
+                tupla_atual = []
+                lex = ''
+                i += 2
+                continue
+            } if (strig_lida[i + 1] == '\r') {
+                inserir_tabela_simbolo(tupla_atual)
+                tupla_atual = []
+                return
+            }
             i += 1
             continue
         }
-        if (retorno[1] == "ERRO" && lex != '') {
-            inserir_tabela_simbolo(retorno)
-            lex = ''
-            i -= 1
-            continue
-        }
-        if (line[i + 1] == '\n' && retorno[1] == "ERRO") {
-            console.log("ERRO")
-            return
-        }
-
-        if (retorno[1] == "ERRO" && line[i + 1] == " ") {
-            console.log("ERRO ESPACO")
-            return
+        
+        if (retorno[1] == "ERRO") {
+            if (tupla_atual.length != 0) {
+                inserir_tabela_simbolo(tupla_atual)
+                tupla_atual = []
+                lex = ''
+            } else {
+                if(strig_lida[i+1] == ' ' && lex[0] != '\"'){
+                    log_erros(linha, i)
+                    lex = ''
+                    i += 2
+                    continue
+                }   
+                if(strig_lida[i+1] == '\r'){
+                    log_erros(linha, i)
+                    return
+                }
+                i+=1
+            }
         }
 
     }
 }
 
-tabela_de_simbolos = {}
+var alphabet = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ").split("");
+var numbers = ("0123456789")
+var dic = {}
 
-// Fazer isso para cada linha do arquivo:
-addLexemas('12345\n')
+for (i in alphabet) {
+    dic[alphabet[i]] = 0
+}
+
+for (i in numbers) {
+    dic[numbers[i]] = 1
+}
+
+dic['.'] = 2
+dic['^'] = 3
+dic['+'] = 4
+dic['-'] = 4
+dic['*'] = 4
+dic['/'] = 4
+dic['\"'] = 5
+dic['_'] = 6
+dic['{'] = 7
+dic['}'] = 8
+dic[' '] = 11
+dic['<'] = 13
+dic['>'] = 14
+dic['='] = 15
+dic['('] = 16
+dic[')'] = 17
+dic[';'] = 18
+
+var tabela_de_transicao = []
+
+var tabela_de_simbolos = {}
+
+//Transições para num
+tabela_de_transicao[[0, 1]] = 1;
+
+tabela_de_transicao[[1, 1]] = 1; //Aceitacao
+tabela_de_transicao[[1, 2]] = 2
+tabela_de_transicao[[1, 3]] = 4
+
+tabela_de_transicao[[2, 1]] = 3
+
+tabela_de_transicao[[3, 1]] = 3 //Aceitacao
+tabela_de_transicao[[3, 3]] = 4
+
+tabela_de_transicao[[4, 1]] = 6
+tabela_de_transicao[[4, 4]] = 5
+
+tabela_de_transicao[[5, 1]] = 6
+
+tabela_de_transicao[[6, 1]] = 6 //Aceitacao
+
+//Transições para literal
+tabela_de_transicao[[0, 5]] = 7
+tabela_de_transicao[[7, 5]] = 8 //Aceitacao
+
+//Transições para identificador
+tabela_de_transicao[[0, 0]] = 9 //Aceitacao
+tabela_de_transicao[[9, 0]] = 9 //Aceitacao
+tabela_de_transicao[[9, 1]] = 9 //Aceitacao
+tabela_de_transicao[[9, 6]] = 9 //Aceitacao
+
+//Transições para comentario
+tabela_de_transicao[[0, 7]] = 10
+tabela_de_transicao[[10, 8]] = 11 //Aceitacao
+
+//Transições para Operadores Relacionais < 13, > 14, = 15
+tabela_de_transicao[[0, 13]] = 14  //Aceitação
+tabela_de_transicao[[0, 15]] = 18  //Aceitação
+tabela_de_transicao[[14, 14]] = 15  //Aceitação
+tabela_de_transicao[[0, 14]] = 16  //Aceitação
+tabela_de_transicao[[14, 15]] = 17  //Aceitação
+tabela_de_transicao[[16, 15]] = 17  //Aceitação
+
+//Transições para Atribuições
+tabela_de_transicao[[0, 13]] = 14  //Aceitação
+tabela_de_transicao[[14, 4]] = 19  //Aceitação
+
+//Transições para Operadores Aritméticos (+,-,*,/)
+tabela_de_transicao[[0, 4]] = 20  //Aceitação
+
+//Transições para Abre parenteses
+tabela_de_transicao[[0, 16]] = 21  //Aceitação
+
+//Transições para fecha parenteses
+tabela_de_transicao[[0, 17]] = 22  //Aceitação
+
+//Transições para ponto e vírgula (;)
+tabela_de_transicao[[0, 18]] = 23  //Aceitação
+
+// Trocar posições com -1 para undefined ou seja nao colocar
+
+inserir_tabela_simbolo(["inicio", "inicio", "-"])
+inserir_tabela_simbolo(["varinicio", "varinicio", "-"])
+inserir_tabela_simbolo(["varfim", "varfim", "-"])
+inserir_tabela_simbolo(["escreva", "escreva", "-"])
+inserir_tabela_simbolo(["leia", "leia", "-"])
+inserir_tabela_simbolo(["se", "se", "-"])
+inserir_tabela_simbolo(["entao", "entao", "-"])
+inserir_tabela_simbolo(["fimse", "fimse", "-"])
+inserir_tabela_simbolo(["fim", "fim", "-"])
+inserir_tabela_simbolo(["inteiro", "inteiro", "-"])
+inserir_tabela_simbolo(["lit", "lit", "-"])
+inserir_tabela_simbolo(["real", "real", "-"])
 
 console.log(tabela_de_simbolos)
-
-
